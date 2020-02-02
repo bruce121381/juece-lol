100a27008b0824ffe6e4145377fbfe3825295938
mengxzh <mengxzh@weilaicheng.com>
2020-02-02 17:22:48 +0800

    juece-lol-limiter

--- limiter.go
+++ limiter.go
@@ -1,1 +1,44 @@
 package main
+
+import "log"
+
+/**
+用 bucket token 算法实现控流
+*/
+type ConnLimiter struct {
+	/**
+	bucket 容量 ---> 大小
+	*/
+	concurrentConn int
+
+	bucket chan int
+}
+
+/**
+构造函数
+*/
+func NewConnLimiter(concurrentConn int) *ConnLimiter {
+	return &ConnLimiter{concurrentConn: concurrentConn, bucket: make(chan int, concurrentConn)}
+}
+
+//GetConn 获取token
+func (cl *ConnLimiter) GetConn() bool {
+	if len(cl.bucket) >= cl.concurrentConn {
+		log.Printf("Reach the rate limitation")
+		return false
+	}
+
+	cl.bucket <- 1
+
+	return true
+
+}
+
+//RelaseConn 释放token
+func (cl *ConnLimiter) ReleaseConn() {
+
+	c := <-cl.bucket
+
+	log.Printf("New Connection coming : %d", c)
+
+}
--- main.go
+++ main.go
@@ -5,6 +5,33 @@ import (
 	"net/http"
 )
 
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if !m.l.GetConn() {
+		sendErrorResponse(w, http.StatusTooManyRequests, "Too many requests")
+		return
+	}
+
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler {
+
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+
+	return m
+
+}
+
 func RegisterHandlers() *httprouter.Router {
 
 	router := httprouter.New()
@@ -20,9 +47,14 @@ func RegisterHandlers() *httprouter.Router {
 func main() {
 
 	r := RegisterHandlers()
+
+	/**
+	注册流控
+	*/
+	mh := NewMiddleWareHandler(r, 2)
 	/**
 	监听9000端口
 	*/
-	http.ListenAndServe(":9000", r)
+	http.ListenAndServe(":9000", mh)
 
 }
