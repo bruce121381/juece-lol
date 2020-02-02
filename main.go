a905ca984b25683ac1357a506b87f5f39b2a4fa8
mengxzh <mengxzh@weilaicheng.com>
2020-02-02 13:06:44 +0800

    juece-streamserver-response

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+
+	router := httprouter.New()
+
+	router.GET("/videos/:vid-id", streamHandler)
+
+	router.POST("/video/:vid-id", uploadHandler)
+
+	return router
+
+}
+
+func main() {
+
+	r := RegisterHandlers()
+	/**
+	监听9000端口
+	*/
+	http.ListenAndServe(":9000", r)
+
+}
