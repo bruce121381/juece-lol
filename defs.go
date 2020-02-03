f9f1385b6ee8216a4eb4de9cf44b86ad2ca0a0f9
mengxzh <mengxzh@weilaicheng.com>
2020-02-03 16:09:37 +0800

    juece-lol-upLoadFiles

--- defs.go
+++ defs.go
@@ -1,5 +1,12 @@
 package main
 
 const (
+	/**
+
+	 */
 	VIDEO_DIR = "./videos/ "
+	/**
+	50M的上传文件最大大小
+	*/
+	MAX_UPLOAD_SIZE = 1024 * 1024 * 50
 )
--- handlers.go
+++ handlers.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+/**
+播放视频
+*/
 func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	videoId := p.ByName("vid-id")
 	videoLink := VIDEO_DIR + videoId
@@ -24,6 +29,40 @@ func streamHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 
 }
 
+/**
+上传视频
+*/
 func uploadHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	//上传视频最大大小
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
 
+	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+		log.Printf("Bad Request, err is %s", err)
+		sendErrorResponse(w, http.StatusBadRequest, "Fail is too large")
+		return
+	}
+
+	file, _, err := r.FormFile("file") //<file name = "file"
+	if err != nil {
+		log.Printf("file read is failed, err is %s", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "failed read file")
+		return
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Printf("Read file is failed, err is %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "Internal error")
+	}
+
+	fileName := p.ByName("vid-id")
+	err = ioutil.WriteFile(VIDEO_DIR+fileName, data, 0666)
+	if err != nil {
+		log.Printf("file write error, err is %v", err)
+		sendErrorResponse(w, http.StatusInternalServerError, "file open failed")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "Upload is success")
 }
